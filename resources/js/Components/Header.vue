5e77bddb37a66a84b3a7f7ae27c0a279c0021620
Tisherd <mtisherd@gmail.com>
2025-02-24 15:38:54 +0800

    create components: Header, CrudTable, Pagination

---
+++ resources/js/Components/Header.vue
@@ -0,0 +1,19 @@
+<script setup>
+import { Link } from '@inertiajs/vue3';
+</script>
+
+<template>
+    <header class="bg-gray-600 text-white p-3 flex justify-between items-center">
+        <ul class="flex space-x-4 pl-20">
+            <li>
+                <Link href="/" class="hover:underline">О проекте</Link>
+            </li>
+            <li>
+                <Link href="/products" class="hover:underline">Товары</Link>
+            </li>
+            <li>
+                <Link href="/orders" class="hover:underline">Заказы</Link>
+            </li>
+        </ul>
+    </header>
+</template>
---
+++ resources/js/Components/Pagination.vue
@@ -0,0 +1,31 @@
+<script setup>
+import { Link } from '@inertiajs/vue3';
+
+defineProps({
+    links: Array,
+});
+</script>
+
+<template>
+    <div v-if="links.length > 3"
+        class="px-4 py-3 flex items-center justify-between sm:px-6">
+        <div class="hidden sm:flex-1 sm:flex sm:items-center sm:justify-between">
+            <div>
+                <nav class="relative z-0 inline-flex rounded-md shadow-sm -space-x-px" aria-label="Pagination">
+
+                    <Link v-for="(link, k) in links"
+                        :href="link.url || '#'"
+                        :key="k"
+                        :disabled="(link.url === null) || (link.active)"
+                        :class="[
+                            link.active ? 'text-blue-500 font-bold pointer-events-none' : 'text-gray-500',
+                            link.url === null ? 'bg-gray-100 pointer-events-none' : 'hover:bg-gray-50',
+                            'bg-white border-gray-300 relative inline-flex items-center px-4 py-2 border text-sm font-medium'
+                        ]"
+                        v-html="link.label"
+                    />
+                </nav>
+            </div>
+        </div>
+    </div>
+</template>
