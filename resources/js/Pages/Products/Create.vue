de01f2705eb6c2eb71491b99c34e38a5ce6501e4
Tisherd <mtisherd@gmail.com>
2025-02-24 16:04:25 +0800

    correct Products/Create.vue

--- resources/js/Pages/Products/Create.vue
+++ resources/js/Pages/Products/Create.vue
@@ -1,33 +1,42 @@
 <script setup>
-import { ref } from 'vue';
-import { router } from '@inertiajs/vue3';
+import { Link, useForm } from '@inertiajs/vue3';
 import MainLayout from '@/Layouts/MainLayout.vue';
+import InputError from '@/Components/Base/InputError.vue';
 
 defineProps({
     categories: Array
 });
 
-const form = ref({
-    name: '',
-    category_id: '',
-    description: '',
-    price: ''
+const form = useForm({
+    name: null,
+    category_id: null,
+    description: null,
+    price: null
 });
 
 const submit = () => {
-    // form.value.price = Math.round(parseFloat(form.value.price) * 100);
-    router.post('/products', form.value);
+    form.post('/products');
 };
 </script>
 
 <template>
     <MainLayout>
-        <div class="container mx-auto p-4">
-            <h1 class="text-2xl font-bold mb-4">Добавить товар</h1>
+        <div class="max-w-3xl mx-auto">
+            <div class="w-full mx-auto py-6 px-4 sm:px-6 lg:px-8">
+                <h1 class="text-3xl font-bold text-gray-900">
+                    Добавить товар
+                </h1>
+
+                <Link href="/products" class="text-indigo-600 hover:text-indigo-900 my-5 block">
+                    Вернуться назад
+                </Link>
+            </div>
+
             <form @submit.prevent="submit" class="space-y-4">
                 <div>
                     <label class="block">Название</label>
                     <input v-model="form.name" required class="border p-2 w-full" />
+                    <InputError class="mt-2" :message="form.errors.name" />
                 </div>
                 <div>
                     <label class="block">Категория</label>
@@ -36,14 +45,17 @@ const submit = () => {
                             {{ category.name }}
                         </option>
                     </select>
+                    <InputError class="mt-2" :message="form.errors.category_id" />
                 </div>
                 <div>
                     <label class="block">Описание</label>
                     <textarea v-model="form.description" class="border p-2 w-full"></textarea>
+                    <InputError class="mt-2" :message="form.errors.description" />
                 </div>
                 <div>
                     <label class="block">Цена (₽)</label>
                     <input v-model="form.price" required type="number" step="0.01" class="border p-2 w-full" />
+                    <InputError class="mt-2" :message="form.errors.price" />
                 </div>
                 <button type="submit" class="bg-green-500 text-white px-4 py-2 rounded">Сохранить</button>
             </form>
