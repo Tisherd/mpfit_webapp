1dd133ba48b00dc003474883f01fe1967b51701e
Tisherd <mtisherd@gmail.com>
2025-02-24 15:38:22 +0800

    add routes to web.php

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,14 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\OrderController;
 
 Route::get('/', function () {
-    return view('welcome');
+    return Inertia::render('AboutProject', []);
 });
+
+Route::resource('products', ProductController::class)->names('products');
+Route::resource('orders', OrderController::class)->names('orders');
