f25a857df5e8af404725601dffab40442f74804d
Tisherd <mtisherd@gmail.com>
2025-02-24 15:38:05 +0800

    create AboutProject page and MainLayout

---
+++ resources/js/Layouts/MainLayout.vue
@@ -0,0 +1,12 @@
+<script setup>
+import Header from "@/Components/Header.vue";
+</script>
+
+<template>
+    <div class="bg-gray-100 text-gray-800 flex flex-col min-h-screen overflow-hidden">
+        <Header />
+        <main class="container mx-auto px-4 py-8 flex-grow">
+            <slot />
+        </main>
+    </div>
+</template>
---
+++ resources/js/Pages/AboutProject.vue
@@ -0,0 +1,17 @@
+<script setup>
+import MainLayout from '@/Layouts/MainLayout.vue';
+import readme from '@/../../README.md?raw';
+import { ref } from 'vue';
+import { marked } from 'marked';
+
+const markdownContent = ref('');
+markdownContent.value = marked(readme);
+</script>
+
+<template>
+    <MainLayout>
+        <div class="prose prose-lg max-w-none p-6 bg-white shadow-md rounded-md">
+            <div v-html="markdownContent"></div>
+        </div>
+    </MainLayout>
+</template>
